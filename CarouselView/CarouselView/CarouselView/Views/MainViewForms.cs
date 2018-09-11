672f240990354fc2d41130427c794a7998d5f0c7
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2018-09-11 10:24:22 -0500

    Live Carousel

---
+++ CarouselView/CarouselView/CarouselView/Views/MainViewForms.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace CarouselView
+{
+    class MainViewForms:ContentPage
+    {
+        private Point? _dimensions;
+        
+        private DateTime _timerStart;
+
+        // Content variable definitions
+       
+        ManualCarouselView box3;
+        
+        TimeWrapper timer;
+        public MainViewForms()
+        {
+            Content = new ContentView();
+            timer = new TimeWrapper(new TimeSpan(0, 0, 1), true, TimerElapsedEvt);
+        }
+        private void TimerElapsedEvt()
+        {
+            var secondsSinceStart = GetSecondsSinceTimerStart;
+            if (secondsSinceStart % 2 == 0)
+            {
+                box3.AdvancePage(1);
+            }
+        }
+
+        private int GetSecondsSinceTimerStart
+        {
+            get
+            {
+                TimeSpan lapsedTime = DateTime.Now - _timerStart;
+                return lapsedTime.Seconds;
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _timerStart = DateTime.Now;
+            timer.Start();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            timer.Stop();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (!_dimensions.HasValue || (_dimensions.Value.X != width && _dimensions.Value.Y != height))
+            {
+                _dimensions = new Point(width, height);
+                Content = GenerateLayout();
+            }
+        }
+
+        private View GenerateLayout()
+        {
+            box3 = new ManualCarouselView { Pages = new List<Layout>() }; // Double-Width Live-Tile
+
+            SetupBox3(box3);
+            
+            Content = new ContentView
+            {
+                Content = box3
+            };
+            return new ScrollView
+            {
+                Orientation = ScrollOrientation.Vertical,
+                Content = Content,
+            };
+        }
+        
+        private void SetupBox3(ManualCarouselView box)
+        {
+            Label lb1 = new Label
+            {
+                TextColor = Color.Black,
+                Text = "Title 4"
+            };
+            Label lb2 = new Label
+            {
+                TextColor = Color.White,
+                Text = "Title 5"
+            };
+            ContentView pg1 = new ContentView
+            {
+                Padding = new Thickness(5),
+                BackgroundColor = Color.FromHex("#92E9DC"),
+                Content = lb1
+            };
+            ContentView pg2 = new ContentView
+            {
+                Padding = new Thickness(5),
+                BackgroundColor = Color.FromHex("#399A8C"),
+                Content = lb2
+            };
+
+            box.Pages.Add(pg1);
+            box.Pages.Add(pg2);
+            box.Initialise(0);
+        }
+
+        
+    }
+}
