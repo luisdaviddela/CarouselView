e69e09a256969b9a50ac0833620b4b463cd85f02
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2018-09-10 22:54:47 -0500

    Carousel Files

---
+++ CarouselView/CarouselView/CarouselView/MainViewForms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace CarouselView
+{
+    class MainViewForms:ContentPage
+    {
+        public MainViewForms()
+        {
+
+        }
+    }
+}
